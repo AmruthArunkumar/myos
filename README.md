b3f2d443334402dd20ef5f287e468b542b36d026
Amruth Arunkumar <amruth1.618@gmail.com>
2025-06-24 00:14:42 -0400

    Intro

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# myos
-building an os from scratch
+# My Operating System
+Building an os from scratch
+
+## Setup
+Install Dependencies
+```bash
+sudo apt-get install g++ binutils libc6-dev-i386 grub-pc xorriso mtools
+```
+Make ISO File
+```bash
+make loader.o
+make kernel.o
+make mykernel.bin
+make mykernel.iso
+```
+Install Virtual Box
+```bash
+wget https://download.virtualbox.org/virtualbox/7.0.20/virtualbox-7.0_7.0.20-163906~Ubuntu~jammy_amd64.deb
+sudo apt --fix-broken install virtualbox-7.0_7.0.20-163906~Ubuntu~jammy_amd64.deb
+```
+Run The OS in the VM
+```bash
+make run
+```
+
+## Intro
+A Simple Hello World OS
--- kernel.cpp
+++ kernel.cpp
@@ -1,10 +1,23 @@
-#include <cstdio>
+void printf(char* str) {
+    unsigned short* VideoMemory = (unsigned short*) 0xB8000;
+    for (int i = 0; str[i] != '\0'; ++i) {
+        VideoMemory[i] = (0x09 << 8) | str[i];
+        // VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
+    }
+}
 
-void kernelMain()
+typedef void (*constructor)();
+extern "C" constructor start_ctors;
+extern "C" constructor end_ctors;
+extern "C" constructor callConstructors() {
+    for (constructor* i = &start_ctors; i != &end_ctors; i++) {
+        (*i)();
+    }
+}
+
+extern "C" void kernelMain(void* multiboot_structure, unsigned int magicnumber)
 {
     printf("Hello World!");
 
-    while (1)
-    {
-    }
+    while (1);
 }
